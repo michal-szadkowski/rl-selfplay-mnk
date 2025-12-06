ff3d86e79424eec1b8e2eea5880d0a8c5b80797d
Michał Szadkowski <michal.szadkowski.0@gmail.com>
2025-12-06 12:02:34 +0100

    Add run config for L networks

---
+++ src/trainl.py
@@ -0,0 +1,25 @@
+import wandb
+
+from train import get_default_config, train_mnk
+
+if __name__ == "__main__":
+    architectures = ["transformer_l", "resnet_l", "cnn_l"]
+
+    for arch in architectures:
+        config = get_default_config()
+        config["architecture_name"] = arch
+        config["batch_size"] = 4096
+
+        if arch == "transformer_l":
+            config["entropy_coef"] = 0.1
+        else:
+            config["entropy_coef"] = 0.04
+
+        with wandb.init(
+            config=config,
+            project="mnk",
+            group="main_run_small_board_l",
+            name=f"run_{arch}",
+            tags=[arch, "main_experiment"],
+        ) as run:
+            train_mnk(run)
