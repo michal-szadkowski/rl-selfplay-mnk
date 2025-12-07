eb98e89f95cb17f60f7b58698ca0a51a9461f82b
Michał Szadkowski <michal.szadkowski.0@gmail.com>
2025-12-07 13:08:46 +0100

    Add training config for sweep

---
+++ src/train_short.py
@@ -0,0 +1,15 @@
+import wandb
+
+from train import get_default_config, train_mnk
+
+if __name__ == "__main__":
+    config = get_default_config()
+
+    config["total_environment_steps"] = 80_000_000
+    config["entropy_coef_schedule"] = {
+        "type": "linear",
+        "params": {"final_coef": 0.001, "total_steps": 50_000_000},
+    }
+
+    with wandb.init(config=config, project="mnk_b_sweeps") as run:
+        train_mnk(run)
