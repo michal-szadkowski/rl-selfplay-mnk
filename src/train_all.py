a27e3a9b54ced33316a4abd7ca348bf62c79f6af
Michał Szadkowski <michal.szadkowski.0@gmail.com>
2025-12-12 18:33:26 +0100

    Configure for main training runs

--- src/train_all.py
+++ src/train_all.py
@@ -3,22 +3,33 @@ import wandb
 from train import get_default_config, train_mnk
 
 if __name__ == "__main__":
-    architectures = ["transformer_b_s", "resnet_b_s", "cnn_b_s"]
+    architectures = [
+        "transformer_b_s",
+        "resnet_b_s",
+        "cnn_b_s",
+        "transformer_b_l",
+        "resnet_b_l",
+        "cnn_b_l",
+    ]
 
     for arch in architectures:
         config = get_default_config()
         config["architecture_name"] = arch
-        config["learning_rate"] = 5e-4
 
         if "transformer" in arch:
-            config["entropy_coef"] = 0.1
-        else:
+            config["entropy_coef"] = 0.12
+            config["learning_rate"] = 12e-4
+        elif "resnet" in arch:
+            config["entropy_coef"] = 0.05
+            config["learning_rate"] = 8e-4
+        elif "cnn" in arch:
             config["entropy_coef"] = 0.04
+            config["learning_rate"] = 6e-4
 
         with wandb.init(
             config=config,
             project="mnk_b",
-            group="main_run_small_board_s",
+            group="main_run2_small_board",
             name=f"run_{arch}",
             tags=[arch, "main_experiment"],
         ) as run:
