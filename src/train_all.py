a05edc4719b272100b8186655a93b7ca9ab3b7a6
Michał Szadkowski <michal.szadkowski.0@gmail.com>
2025-12-20 13:29:25 +0100

    Experiment with final coef parameter

--- src/train_all.py
+++ src/train_all.py
@@ -5,16 +5,15 @@ from train import get_default_config, train_mnk
 if __name__ == "__main__":
     architectures = [
         "transformer_b_s",
-        "resnet_b_s",
+        "resnset_b_s",
         "cnn_b_s",
-        "transformer_b_l",
-        "resnet_b_l",
-        "cnn_b_l",
     ]
 
     for arch in architectures:
         config = get_default_config()
         config["architecture_name"] = arch
+
+        config["entropy_coef_schedule"]["params"]["final_coef"] = 0.01
 
         if "transformer" in arch:
             config["entropy_coef"] = 0.10
@@ -30,7 +29,8 @@ if __name__ == "__main__":
             config=config,
             project="mnk_b",
             group="main_run2_small_board",
-            name=f"run_{arch}",
+            name=f"run4_{arch}",
             tags=[arch, "main_experiment"],
+            notes="Zwiększony parametr entropy final_coef",
         ) as run:
             train_mnk(run)
