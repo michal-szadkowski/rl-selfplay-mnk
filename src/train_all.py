c3bcb7b97a191c704157e79b9f8d0fff64dc282c
Michał Szadkowski <michal.szadkowski.0@gmail.com>
2025-12-22 12:02:18 +0100

    Set entropy coef schedule parameters for all networks

--- src/train_all.py
+++ src/train_all.py
@@ -5,23 +5,27 @@ from train import get_default_config, train_mnk
 if __name__ == "__main__":
     architectures = [
         "transformer_b_s",
-        "resnset_b_s",
+        "resnet_b_s",
         "cnn_b_s",
+        "transformer_b_l",
+        "resnet_b_l",
+        "cnn_b_l",
     ]
 
     for arch in architectures:
         config = get_default_config()
         config["architecture_name"] = arch
 
-        config["entropy_coef_schedule"]["params"]["final_coef"] = 0.01
-
         if "transformer" in arch:
+            config["entropy_coef_schedule"]["params"]["final_coef"] = 0.01
             config["entropy_coef"] = 0.10
             config["learning_rate"] = 12e-4
         elif "resnet" in arch:
+            config["entropy_coef_schedule"]["params"]["final_coef"] = 0.001
             config["entropy_coef"] = 0.05
             config["learning_rate"] = 8e-4
         elif "cnn" in arch:
+            config["entropy_coef_schedule"]["params"]["final_coef"] = 0.001
             config["entropy_coef"] = 0.04
             config["learning_rate"] = 6e-4
 
